09f1ba5478744a1fc1982b6a2fe949e7f041f56b
Carlos <carlos.marangoni1@gmail.com>
2021-09-08 21:54:21 -0300

    Adicionando footer,navbar e estrutura inicial

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'
+import { rootRouterConfig } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+
+import {MenubarModule} from 'primeng/menubar';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rootRouterConfig),
+    MenubarModule
+  ],
+  exports:[
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
