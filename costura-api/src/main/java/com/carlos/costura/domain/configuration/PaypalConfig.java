587962a92a4eba5bc60f325737cf03c3284560ac
Carlos <carlos.marangoni1@gmail.com>
2021-10-21 14:13:56 -0300

    Romaneio CRUD - Transformando endereco em latitude e longitude

---
+++ costura-api/src/main/java/com/carlos/costura/domain/configuration/PaypalConfig.java
@@ -0,0 +1,90 @@
+package com.carlos.costura.domain.configuration;
+
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+public class PaypalConfig {
+
+    @Value("${paypal.client.id}")
+    private String clientId;
+
+    @Value("${paypal.client.secret}")
+    private String clientSecret;
+
+    @Value("${paypal.mode}")
+    private String mode;
+
+    public Map<String, Object> createPayment(String sum){
+        Map<String, Object> response = new HashMap<String, Object>();
+        Amount amount = new Amount();
+        amount.setCurrency("BRL");
+        amount.setTotal(sum);
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+        List<Transaction> transactions = new ArrayList<Transaction>();
+        transactions.add(transaction);
+
+        Payer payer = new Payer();
+        payer.setPaymentMethod("paypal");
+
+        Payment payment = new Payment();
+        payment.setIntent("sale");
+        payment.setPayer(payer);
+        payment.setTransactions(transactions);
+
+        RedirectUrls redirectUrls = new RedirectUrls();
+        redirectUrls.setCancelUrl("http://localhost:4200/cancel");
+        redirectUrls.setReturnUrl("http://localhost:4200/");
+        payment.setRedirectUrls(redirectUrls);
+        Payment createdPayment;
+        try {
+            String redirectUrl = "";
+            APIContext context = new APIContext(clientId, clientSecret, "sandbox");
+            createdPayment = payment.create(context);
+            if(createdPayment!=null){
+                List<Links> links = createdPayment.getLinks();
+                for (Links link:links) {
+                    if(link.getRel().equals("approval_url")){
+                        redirectUrl = link.getHref();
+                        break;
+                    }
+                }
+                response.put("status", "success");
+                response.put("redirect_url", redirectUrl);
+            }
+        } catch (PayPalRESTException e) {
+            System.out.println("Error happened during payment creation!");
+        }
+        return response;
+    }
+
+    public Map<String, Object> completePayment(HttpServletRequest req){
+        Map<String, Object> response = new HashMap();
+        Payment payment = new Payment();
+        payment.setId(req.getParameter("paymentId"));
+
+        PaymentExecution paymentExecution = new PaymentExecution();
+        paymentExecution.setPayerId(req.getParameter("PayerID"));
+        try {
+            APIContext context = new APIContext(clientId, clientSecret, "sandbox");
+            Payment createdPayment = payment.execute(context, paymentExecution);
+            if(createdPayment!=null){
+                response.put("status", "success");
+                response.put("payment", createdPayment);
+            }
+        } catch (PayPalRESTException e) {
+            System.err.println(e.getDetails());
+        }
+        return response;
+    }
+}
